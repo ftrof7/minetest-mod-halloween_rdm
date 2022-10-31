d4f1610d771a4dc6e0e8ea657e5bdfe1c60a709a
ftrof <112196266+ftrof7@users.noreply.github.com>
2022-10-31 13:24:39 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -77,7 +77,7 @@ minetest.register_craft({
 	recipe = {
 		{"default:gold_ingot","default:gold_ingot","default:gold_ingot"},	
 		{"default:gold_ingot","default:gold_ingot","default:gold_ingot"},
-		{"default:gold_ingot","default:gold_ingot","default:gold_ingot"},			
+		{"default:gold_ingot","default:gold_ingot"},			
 	},
 })
 
